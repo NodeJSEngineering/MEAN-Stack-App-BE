c092743f4d7fba6cf2f47a79110e44a6d8b479a4
Gupta, Sunny <sunnyvales789@gmail.com>
2024-10-23 00:16:11 +0530

    update

--- mongo-demo.js
+++ mongo-demo.js
@@ -134,3 +134,32 @@ MongoClient.connect("mongodb://localhost:27017", (err, client) => {
 
   
 });
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+
+const app=express()
+
+dotenv.config()
+
+const connect = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO_TOKEN);
+      console.log("Connected to Database");
+    } catch (e) {
+      throw e;
+    }
+  };
+
+  mongoose.connection.on("connected", () => {
+    console.log("mongodb connected");
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("mongodb disconnected");
+  });
+
+app.listen(5000, () => {
+    connect();
+    console.log("server listening on port number 5000");
+  });
