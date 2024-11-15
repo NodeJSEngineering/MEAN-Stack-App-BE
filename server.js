b5bead8ab484d35ef26f0a252f3518f5ac9b7879
Gupta, Sunny <sunnyvales789@gmail.com>
2024-11-15 10:09:02 +0530

    update

--- server.js
+++ server.js
@@ -33,14 +33,26 @@ mongoose
 const app = express();
 app.use(bodyParser.json());
 
-// app.use((req, res, next) =>{
-//   res.header("Access-Control-Allow-origin", "*")
-//   res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE")
-//   res.header("Access-Control-Allow-Headers", "Origin",
-//   "X-Requested-With", "Content-Type", "Accept")
-//   next()
-//   })
-  app.use(cors());
+app.use((req, res, next) =>{
+  res.header("Access-Control-Allow-origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE")
+  res.header("Access-Control-Allow-Headers", "Origin",
+  "X-Requested-With", "Content-Type", "Accept")
+  next()
+  })
+
+  app.use((req, res, next) => {
+    console.log('Time:', Date.now())
+    next()
+    })
+
+    // This example shows a middleware function mounted on the /user/:id path. The function is executed for any type of HTTP request on the /user/:id path.
+
+app.use('/user/:id', (req, res, next) => {
+console.log('Request Type:', req.method)
+next()
+})
+  // app.use(cors());
   // var corsOptions = {
   //   origin: "http://localhost:8081"
   // };
